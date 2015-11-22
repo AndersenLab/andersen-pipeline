51200e95057fef03f7e0528b8429deb32b8b898d
danielecook <danielecook@gmail.com>
2015-11-21 22:35:51 -0600

    add SP filter for merging

--- pipe/scripts/conc_merge_union.sh
+++ pipe/scripts/conc_merge_union.sh
@@ -2,5 +2,6 @@
 # CHECK: {vcf_path}/snps_concordance/union_merged.bcftools.vcf.gz
 # CHECK: {vcf_path}/snps_concordance/union_merged.bcftools.vcf.gz.csi
 bcftools merge -m all --apply-filters PASS -O v `ls {vcf_path}/snps_concordance/*.union.filtered.bcftools.vcf.gz` | \
+bcftools view --exclude 'FORMAT/SP > 0' --set-GTs . | \
 bcftools view -X -O z > {vcf_path}/snps_concordance/union_merged.bcftools.vcf.gz
 bcftools index {vcf_path}/snps_concordance/union_merged.bcftools.vcf.gz
